983fc77f369ed8c39503f21c4416f31adf1e4e4c
yongbin <rhdydqls1024@naver.com>
2022-08-04 13:00:42 +0900

    생성자 생성

--- src/main/java/com/study/security/domain/user/service/UserDetailsImpl.java
+++ src/main/java/com/study/security/domain/user/service/UserDetailsImpl.java
@@ -10,11 +10,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-@Getter @AllArgsConstructor
-@RequiredArgsConstructor
+@Getter
 public class UserDetailsImpl implements UserDetails {
 
     private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
